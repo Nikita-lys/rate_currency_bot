034e429076720e9e958be5b5d858eacc451f979d
Nikita-lys <lysenkonikita61@mail.ru>
2018-12-26 18:44:57 +0300

    initial

---
+++ main.rb
@@ -0,0 +1,26 @@
+require 'telegram/bot'
+
+TOKEN = '771018612:AAEwpf6xMHFLej0EY4L2E9kTXYGur76RPt4'
+
+ANSWERS = [
+	"окей",
+	"не окей",
+	"так себе"
+]
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+	bot.listen do |message|
+		case message.text
+		when '/start', '/start start'
+			bot.api.send_message(
+				chat_id: message.chat.id,
+				text: "Здравствуй, #{message.from.first_name}"
+				)
+		else
+			bot.api.send_message(
+				chat_id: message.chat.id,
+				text: ANSWERS.sample
+				)
+		end
+	end
+end
