1241c73bc0496db23ecb1099ba6547a91ff0ab57
Nikita-lys <lysenkonikita61@mail.ru>
2018-12-26 19:09:13 +0300

    initial

--- main.rb
+++ main.rb
@@ -5,10 +5,11 @@ TOKEN = '771018612:AAEwpf6xMHFLej0EY4L2E9kTXYGur76RPt4'
 ANSWERS = [
 	"окей",
 	"не окей",
-	"так себе"
+	"так себе",
+	"ещё один вариант"
 ]
 
-Telegram::Bot::Client.run(TOKEN) do |bot|
+Telegram::Bot::Client.run(token) do |bot|
 	bot.listen do |message|
 		case message.text
 		when '/start', '/start start'
